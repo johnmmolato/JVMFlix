0d8aff5cb4fe5ab2cd3903d8156068d1897bd249
Vanessa Constant <vaconstant@gmail.com>
2022-08-14 19:19:52 -0400

    Created enums and classes

--- src/com/jvmflix/movie/Genre.java
+++ src/com/jvmflix/movie/Genre.java
@@ -1,4 +1,16 @@
 package com.jvmflix.movie;
 
 enum Genre {
-}
+
+    COMEDY,
+    ACTION,
+    ROMANCE,
+    HORROR,
+    DRAMA,
+    SCIENCE_FICTION,
+    FANTASY,
+    CHILDRENS_FILM,
+    ANIMATION,
+    DOCUMENTARY
+
+    }
---
+++ src/com/jvmflix/movie/JVMFlixApp.java
@@ -0,0 +1,4 @@
+package com.jvmflix.movie;
+
+class JVMFlixApp{
+}
