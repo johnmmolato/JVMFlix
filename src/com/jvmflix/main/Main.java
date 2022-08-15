935c0bee492f1daeeacfbce311cad705d94a268b
java.miggie <miggiegaricadev@gmail.com>
2022-08-15 11:47:28 -0400

    movie list updated with interested and removed sources

--- src/com/jvmflix/main/Main.java
+++ src/com/jvmflix/main/Main.java
@@ -1,7 +1,11 @@
 package com.jvmflix.main;
 
-class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+class Main {
+    public static void main(String[] args)  {
+    //testing file reading (Miggie)
+     //   File file = new File ("/Users/java.miggie/AmazonSDE/01%20-%20Java/StudentWork/MiniProject/JVMFlix/src/com/jvmflix/movie/moviesList.txt");
+     //   Scanner scan = new Scanner(file);
     }
 }
