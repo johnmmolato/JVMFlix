c49dd264ca5a4724d3139a93004e1022578e52d2
Vanessa Constant <vaconstant@gmail.com>
2022-08-16 15:59:22 -0400

    completed minorUser, and downlaod method

--- src/com/jvmflix/user/UserClient.java
+++ src/com/jvmflix/user/UserClient.java
@@ -2,18 +2,31 @@ package com.jvmflix.user;
 
 import com.jvmflix.movie.Genre;
 import com.jvmflix.movie.Interest;
+import com.jvmflix.movie.Movie;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 public class UserClient {
     public static void main(String[] args) {
 
         User user1 = new User("Miggie", 21, Genre.HORROR);
+        MinorUser user2 = new MinorUser("Vanessa", 17, Genre.HORROR);
         try {
-            System.out.println(user1.suggestedList(Interest.ANIMALS));
+            List<Movie> movieListResult1 = user1.suggestedList(Interest.ANIMALS);
+            for(Movie movie: movieListResult1){
+                System.out.println(movie);
+            }
             System.out.println(user1.toSelect("Dumb & Dumber"));
             user1.watch();
+            System.out.println();
+
+            List<Movie> movieListResult2 = user2.suggestedList(Interest.ANIMALS);
+            for(Movie movie: movieListResult2){
+                System.out.println(movie);
+            }
+
 
         } catch (IOException e) {
             e.printStackTrace();
