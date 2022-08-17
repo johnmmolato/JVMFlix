58389f62940896444974c8dbdfb32f3bdc4171fa
Vanessa Constant <vaconstant@gmail.com>
2022-08-17 00:37:16 -0400

    Added factory class

--- src/com/jvmflix/movie/UserDatabase.java
+++ src/com/jvmflix/movie/UserDatabase.java
@@ -1,50 +1,50 @@
-package com.jvmflix.movie;
-
-import org.jetbrains.annotations.*;
-
-import java.util.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
-import java.nio.file.Path;
-import java.nio.file.Files;
-import java.nio.charset.Charset;
-import java.io.IOException;
-
-public class UserDatabase {
-
-	public static void main(String[] args) {
-		ArrayList<String> movies = readFromFile("watchlist.txt");
-		System.out.println(Arrays.toString(movies.toArray()));
-		Collections.sort(movies);
-		System.out.println(Arrays.toString(movies.toArray()));
-		saveToFile("watchlist.txt", movies);
-	}
-
-	public static @NotNull
-	Arraylist readFromFile(String fileName){
-		ArrayList<String> userList = new ArrayList<>();
-		try{
-			Scanner scanner = new Scanner(new File(fileName));
-			while (scanner.hasNextLine()){
-				userList.add(scanner.nextLine());
-			}
-			scanner.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return userList;
-	}
-
-	public static void saveToFile(String fileName, ArrayList list) {
-		 Path filePath = Path.get(fileName);
-		try{
-			Files.write(filePath, list, Charset.defaultCharset());
-		} catch (IOException e){
-			e.printStackTrace();
-		}
-	}
-}
+//package com.jvmflix.movie;
+//
+//import org.jetbrains.annotations.*;
+//
+//import java.util.*;
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Collections;
+//import java.util.Scanner;
+//import java.nio.file.Path;
+//import java.nio.file.Files;
+//import java.nio.charset.Charset;
+//import java.io.IOException;
+//
+//public class UserDatabase {
+//
+//	public static void main(String[] args) {
+//		ArrayList<String> movies = readFromFile("watchlist.txt");
+//		System.out.println(Arrays.toString(movies.toArray()));
+//		Collections.sort(movies);
+//		System.out.println(Arrays.toString(movies.toArray()));
+//		saveToFile("watchlist.txt", movies);
+//	}
+//
+//	public static @NotNull
+//	Arraylist readFromFile(String fileName){
+//		ArrayList<String> userList = new ArrayList<>();
+//		try{
+//			Scanner scanner = new Scanner(new File(fileName));
+//			while (scanner.hasNextLine()){
+//				userList.add(scanner.nextLine());
+//			}
+//			scanner.close();
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//		return userList;
+//	}
+//
+//	public static void saveToFile(String fileName, ArrayList list) {
+//		 Path filePath = Path.get(fileName);
+//		try{
+//			Files.write(filePath, list, Charset.defaultCharset());
+//		} catch (IOException e){
+//			e.printStackTrace();
+//		}
+//	}
+//}
--- src/com/jvmflix/user/User.java
+++ src/com/jvmflix/user/User.java
@@ -13,15 +13,12 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
+
 
 
 
 public class User {
+
 
     //PROPERTIES
     private String name;
@@ -40,13 +37,13 @@ public class User {
     }
 
     //BUSINESS METH0DS
-    public Movie toSelect( String title) throws IOException {
+    public Movie toSelect(String title) throws IOException {
         List<Movie> selectedMovie = new ArrayList<>();
 
         List<Movie> suggestedMovies = suggestedList(getInterest());
-        for(Movie movie:suggestedMovies){
-            if(title.equals(movie.getTitle())){
-            selectedMovie.add(movie);
+        for (Movie movie : suggestedMovies) {
+            if (title.equals(movie.getTitle())) {
+                selectedMovie.add(movie);
             }
         }
         setSelectedMovie(selectedMovie.get(0));
@@ -57,109 +54,108 @@ public class User {
     public void watch() {
         Movie movie = getSelectedMovie();
         movie.play();
-
-
-    }
-
-    public void saveOnComputer (){
-       Movie movie = getSelectedMovie();
-       movie.download();
     }
 
 
 
-    public List<Movie> videoList( ) throws IOException {
+        public void saveOnComputer() {
+            Movie movie = getSelectedMovie();
+            movie.download();
+        }
+
+
+        public List<Movie> videoList() throws IOException {
 
         /* Pulling array of recommended movies here.
         Scanner is going to read the list of movies text file, filtering by interest and
         *Or statement*
          */
-        List<String> lines =Files.readAllLines(Path.of("movieCollectionNew.csv"));
-        List<Movie> entireDatabase = new ArrayList<>();
+            List<String> lines = Files.readAllLines(Path.of("movieCollectionNew.csv"));
+            List<Movie> entireDatabase = new ArrayList<>();
 
-        for(String line: lines){
+            for (String line : lines) {
 
-            String [] properties = line.split(",");
-
-
-            int id = Integer.parseInt(properties[0]);
-            String title = properties[1];
-            Genre genre = Genre.valueOf(properties[2]);
-            int dateRelease = Integer.parseInt(properties[3]);
-            Rating rating = Rating.valueOf(properties[4]);
-            Interest interest = Interest.valueOf(properties[5]);
-
-            Movie movie = new Movie(id, title, genre, dateRelease, rating, interest);
-            entireDatabase.add(movie);
+                String[] properties = line.split(",");
 
 
-        }
+                int id = Integer.parseInt(properties[0]);
+                String title = properties[1];
+                Genre genre = Genre.valueOf(properties[2]);
+                int dateRelease = Integer.parseInt(properties[3]);
+                Rating rating = Rating.valueOf(properties[4]);
+                Interest interest = Interest.valueOf(properties[5]);
+
+                Movie movie = new Movie(id, title, genre, dateRelease, rating, interest);
+                entireDatabase.add(movie);
+
+
+            }
 
             return entireDatabase;
 
-    }
-
-    public List<Movie> suggestedList(Interest interest) throws IOException{
-        List<Movie> suggested = new ArrayList<>();
-        List<Movie> database = videoList();
-
-        for(Movie movie: database){
-            if(movie.getInterest() == interest || movie.getGenre() == getGenre()){
-                suggested.add(movie);
-
-            }
         }
 
-        setInterest(interest);
+        public List<Movie> suggestedList(Interest interest) throws IOException {
+            List<Movie> suggested = new ArrayList<>();
+            List<Movie> database = videoList();
 
-        return suggested;
+            for (Movie movie : database) {
+                if (movie.getInterest() == interest || movie.getGenre() == getGenre()) {
+                    suggested.add(movie);
+
+                }
+            }
+
+            setInterest(interest);
+
+            return suggested;
+        }
+
+
+        //ACCESSOR METHODS
+        public String getName() {
+            return name;
+        }
+
+        public void setName (String name){
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age){
+            this.age = age;
+        }
+
+        public Genre getGenre() {
+            return genre;
+        }
+
+        public void setGenre(Genre genre){
+            this.genre = genre;
+        }
+
+        public Interest getInterest() {
+            return interest;
+        }
+
+        public void setInterest(Interest interest){
+            this.interest = interest;
+        }
+
+        public Movie getSelectedMovie () {
+            return selectedMovie;
+        }
+
+        public void setSelectedMovie (Movie selectedMovie){
+            this.selectedMovie = selectedMovie;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s: name=%s, age=%s, genre=%s",
+                    getClass().getSimpleName(), getName(), getAge(), getGenre());
+        }
     }
-
-
-    //ACCESSOR METHODS
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Genre getGenre() {
-        return genre;
-    }
-
-    public void setGenre(Genre genre) {
-        this.genre = genre;
-    }
-
-    public Interest getInterest() {
-        return interest;
-    }
-
-    public void setInterest(Interest interest) {
-        this.interest = interest;
-    }
-
-    public Movie getSelectedMovie() {
-        return selectedMovie;
-    }
-
-    public void setSelectedMovie(Movie selectedMovie) {
-        this.selectedMovie = selectedMovie;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%s: name=%s, age=%s, genre=%s",
-                getClass().getSimpleName(), getName(), getAge(), getGenre());
-    }
-}
